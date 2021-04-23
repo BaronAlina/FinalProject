a0baf0da1abc4503e8ac1aba00514c2dfb6fff85
Alina <alinebaron@yandex.com>
2021-04-23 21:11:14 +0500

    Changes

--- app/src/main/java/com/example/finalproject/FirstFragment.java
+++ app/src/main/java/com/example/finalproject/FirstFragment.java
@@ -1,32 +1,86 @@
 package com.example.finalproject;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class FirstFragment extends Fragment {
 
-    LinearLayout fragment1;
-
+    TextView textView;
+    String text;
+    LinearLayout layout;
+    View view;
+    FloatingActionButton fab;
     public void Permission(String permission){
 //        fragment1.findViewById(R.id.fragment);
+        text = permission+"";
     }
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Context context= getActivity().getApplicationContext();
+        final Dialog dialog=new Dialog(context);
+        dialog.setContentView(R.layout.dialog_layout);
+        layout = new LinearLayout(context);
+        view= inflater.inflate(R.layout.fragment_first, null, false);
+        fab=container.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText editText=dialog.findViewById(R.id.edText);
+                Button button=dialog.findViewById(R.id.btn);
+                Button button1=dialog.findViewById(R.id.btn1);
+                button1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        text=editText.getText().toString();
+                        layout.findViewById(R.id.list);
+                        layout.addView(view);
+                        textView=view.findViewById(R.id.text);
+                        textView.setOnCreateContextMenuListener(FirstFragment.this);
+                        textView.setText(text);
+                        dialog.dismiss();
+                    }
+                });
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.setCancelable(false);
+                dialog.show();
+            }
+        });
 
+//        LinearLayout layout=find
+//        LinearLayout fragment1 = new LinearLayout(MainActivity.this)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        return view;//inflater.inflate(R.layout.fragment_first, container, false);
     }
+
+//    @Override
+//    public void onCreateContextMenu(@NonNull ContextMenu menu, @NonNull View v, @Nullable ContextMenu.ContextMenuInfo menuInfo) {
+//        switch (v.getId()){
+//            case R.id.text:
+//        }
+//        super.onCreateContextMenu(menu, v, menuInfo);
+//    }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,23 +1,43 @@
 package com.example.finalproject;
 
 import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.fragment.NavHostFragment;
 
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    String permission;
+    TextView textView;
+    String permission,
+            editName="edit name",
+            editTest="edit test";
+    final int EDIT_NAME=1;
+    final int EDIT_TEST=2;
+    final int DELETE=3;
+    LinearLayout layout;
+    View view1;
+    final FirstFragment fragment1=new FirstFragment();
+    SecondFragment fragment2=new SecondFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,32 +46,105 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final FirstFragment fragment1=new FirstFragment();
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                permission = "YES";
-                fragment1.Permission(permission);
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-                final Dialog dialog=new Dialog(MainActivity.this);
-                dialog.setContentView(R.layout.dialog_layout);
+//        LayoutInflater itInflater=getLayoutInflater();
+//        View view = itInflater.inflate(R.layout.text, null, false);
+//        ViewGroup.LayoutParams lp = view.getLayoutParams();
+//        LinearLayout linLayout = (LinearLayout) findViewById(R.id.fragment);
+//        linLayout.addView(view);
 
-                Button button=dialog.findViewById(R.id.btn);
+
+//        final Dialog dialog=new Dialog(MainActivity.this);
+//        dialog.setContentView(R.layout.dialog_layout);
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+////                permission = "YES";
+////                fragment1.Permission(permission);
+////                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+////                        .setAction("Action", null).show();
+//                final EditText editText=dialog.findViewById(R.id.edText);
+//                Button button=dialog.findViewById(R.id.btn);
+//                Button button1=dialog.findViewById(R.id.btn1);
+//                button1.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        permission=editText.getText().toString();
+//                        LayoutInflater inflater=getLayoutInflater();
+//                        layout = findViewById(R.id.list);
+//                        view1 = inflater.inflate(R.layout.fragment_first, null, false);
+//                        layout.addView(view1);
+//                        textView = view1.findViewById(R.id.text);
+//                        textView.setOnCreateContextMenuListener(MainActivity.this);
+//                        textView.setText(permission);
+//                        fragment1.Permission(permission);
+//                        dialog.dismiss();
+//                    }
+//                });
+//                button.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        dialog.dismiss();
+//                    }
+//                });
+//                dialog.setCancelable(false);
+//                dialog.show();
+//            }
+//        });
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        switch (v.getId()){
+            case R.id.text:
+                menu.add(0, EDIT_NAME, 0, "edit name");
+                menu.add(1, EDIT_TEST, 1, "edit test");
+                menu.add(2, DELETE, 2, "delete test");
+                break;
+        }
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        final Dialog dialog1=new Dialog(MainActivity.this);
+        dialog1.setContentView(R.layout.dialog_layout);
+
+        switch (item.getItemId()){
+            case EDIT_NAME:
+                final EditText editText=dialog1.findViewById(R.id.edText);
+                Button button=dialog1.findViewById(R.id.btn);
+                Button button1=dialog1.findViewById(R.id.btn1);
+                button1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        permission=editText.getText().toString();
+                        textView.setText(permission);
+                        dialog1.dismiss();
+                    }
+                });
                 button.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        dialog.dismiss();
+                        dialog1.dismiss();
                     }
                 });
-                dialog.setCancelable(false);
+                dialog1.setCancelable(false);
+                dialog1.show();
+                break;
+            case EDIT_TEST:
+                permission="yes";
+//                intent.putExtra("yes", permission);
+//                intent.setClass(MainActivity.this, SecondFragment.class);
+//                startActivity(intent);
+                break;
+            case DELETE:
+                layout.removeView(view1);
+                break;
+        }
 
-                dialog.show();
-            }
-        });
-
+        return super.onContextItemSelected(item);
     }
 
     @Override
